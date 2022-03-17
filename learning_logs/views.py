af454433eaab0c4c811a9b1fd1393b2f15f43e66
yawok <obengky72@gmail.com>
2022-03-17 20:24:23 +0000

    refactored views.py.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -23,8 +23,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     #Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -77,9 +76,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
-
+    check_topic_owner(topic, request)
 
     if request.method != "POST":
         #Initial request; pre-fill the form with the current entry.
@@ -93,3 +90,9 @@ def edit_entry(request, entry_id):
 
     context = {"entry": entry, "topic": topic, "form": form}
     return render(request, "learning_logs/edit_entry.html", context)
+
+
+def check_topic_owner(topic, request):
+    """Check if topic belongs to the requesting user."""
+    if topic.owner != request.user:
+        raise Http404
